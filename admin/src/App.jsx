fe242343b6d4b14fc5b5b266fa3e09ed1c631695
forpcpurpose3-sudo <forpcpurpose3@gmail.com>
2026-02-07 13:40:28 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,27 +1,32 @@
+import React, { useContext } from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Add from './pages/Add'
+import Lists from './pages/Lists'
+import Orders from './pages/Orders'
+import Login from './pages/Login'
+import { adminDataContext } from './context/AdminContext'
+  import { ToastContainer, toast } from 'react-toastify';
+
 function App() {
-  const { adminData, loading } = useContext(adminDataContext);
-
-  if (loading) {
-    return (
-      <div className="w-screen h-screen flex items-center justify-center text-xl">
-        Checking admin session...
-      </div>
-    );
-  }
-
+  let {adminData} = useContext(adminDataContext)
   return (
+
     <>
       <ToastContainer />
-      {!adminData ? (
-        <Login />
-      ) : (
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/add" element={<Add />} />
-          <Route path="/lists" element={<Lists />} />
-          <Route path="/orders" element={<Orders />} />
-        </Routes>
-      )}
+    {!adminData ? <Login/> : <>
+
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/add' element={<Add/>}/>
+        <Route path='/lists' element={<Lists/>}/>
+        <Route path='/orders' element={<Orders/>}/>
+        <Route path='/login' element={<Login/>}/>
+      </Routes>
+      </>
+      }
     </>
-  );
+  )
 }
+
+export default App
