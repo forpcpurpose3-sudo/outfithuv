5ffe98ebf8ef5d2efd4b2d489725acddc0616ddb
forpcpurpose3-sudo <forpcpurpose3@gmail.com>
2026-02-07 12:35:50 +0530

    Update AuthContext.jsx

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { createContext } from 'react'
 export const authDataContext= createContext()
 function AuthContext({children}) {
-    let serverUrl = "https://api.theoutfithuv.in/"
+    let serverUrl = "https://outfithuv-backened.onrender.com/"
 
     let value = {
        serverUrl
